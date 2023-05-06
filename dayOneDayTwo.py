e4b73586ec093b5e714d8638565829e7cf684d35
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-05-06 15:37:04 -0500

    initial commit

---
+++ dayOneDayTwo.py
@@ -0,0 +1,8 @@
+# - Day 1-2: Basics of Python (variables, data types, control structures)
+    # - Practice Exercise: Write a Python program that takes two numbers as input from the user and displays their sum, difference, product, and quotient.
+
+userInputOne = int(input('First Integer is...'))
+userInputTwo = int(input('Second Integer is...'))
+sum = userInputOne + userInputTwo
+
+print("Your sum is...", sum)

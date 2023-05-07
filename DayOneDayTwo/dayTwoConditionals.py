0c9f5f9beeeb76cba15cca869f85430a261209f9
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-05-07 17:47:16 -0500

    practice conditionals

---
+++ DayOneDayTwo/dayTwoConditionals.py
@@ -0,0 +1,39 @@
+# if statement
+# birthYear = int(input("what is your birth year? "))
+
+# if birthYear <= 2006:
+#     print("legal")
+
+# if...else statement
+# birth_year = int(input("what is your birth year? "))
+
+# if birth_year <= 2006:
+#     print("legal")
+# elif birth_year > 2006:
+#     print("too young")
+
+# else
+# temp = str(input("One-word weather description: ").lower())
+
+# if temp == "hot":
+    # print("NOW PLAYING...\'Hot In Herre'")
+# elif temp == "cold":
+    # print("NOW PLAYING...\'Ice Ice Baby'")
+# else:
+    # print("NOW PLAYING...\'It\'s A Beautiful Day In The Neighbhorhood'")
+
+# ternary operations
+
+# if statement
+# birthYear = int(input("what is your birth year? "))
+# if birthYear <= 2006: print('legal') 
+
+# if...else statement
+# birth_year = int(input("what is your birth year? "))
+
+# print('legal') if birth_year <= 2006 else print("too young")
+
+# else
+temp = str(input("One-word weather description: ").lower()) 
+print("NOW PLAYING...\'Hot In Herre'") if temp == "hot" else print("NOW PLAYING...\'Ice Ice Baby'") if temp == "cold" else print("NOW PLAYING...\'It\'s A Beautiful Day In The Neighbhorhood'")
+

645a049aacf9da1c72e16f3fc8550d9e17479690
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-05-15 16:10:40 -0500

    practice for Python functions

--- DayThreeDayFour/dayThreeDayFour.py
+++ DayThreeDayFour/dayThreeDayFour.py
@@ -16,3 +16,9 @@ def third_function(*kids):
     print("The youngest is " + kids[-1])
 
 third_function("Ife", "Toyin", "Toyo", "Tito")
+
+#Arbitrary Keyword arguments(**kargs)
+def third_func(**player):
+    print("first place: " + player["first"] + ", second place: " + player["second"] + ", and third place: " + player["third"])
+
+third_func(first = "Jaime", second = "Saheed", third = "Uche")

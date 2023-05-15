750ac32962e017c894af4264ac4e1b7fb055056b
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-05-15 14:19:56 -0500

    practice for Python functions

---
+++ DayThreeDayFour/dayThreeDayFour.py
@@ -0,0 +1,12 @@
+# Defining a Function
+def a_function():
+    print("I just defined a function!")
+
+# Calling a function
+a_function()
+
+# Function arguments
+def another_function(fruit):
+    print("I love " + fruit)
+
+another_function("oranges")
